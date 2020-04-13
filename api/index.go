3a5c52845eb23458655cdc10043796c62dd2fdc7
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-13 23:15:12 +0200

    Update index.go

--- api/index.go
+++ api/index.go
@@ -11,9 +11,8 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true}`)
 	w.WriteHeader(http.StatusOK)
-}
 
-func TestFauna(w http.ResponseWriter, r *http.Request) {
 	secret := os.Getenv("FAUNADB_SECRET")
 	log.Println("Secret: " + secret)
+
 }
