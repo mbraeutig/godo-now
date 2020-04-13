332a6d27a164a68bfd91e6b3bd32a25a5a53586a
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-13 23:20:00 +0200

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,12 +1,11 @@
 package api
 
 import (
-	"io"
 	"net/http"
 	"os"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	secret := os.Getenv("FAUNADB_SECRET")
-	io.WriteString(w, "Secret: "+secret)
+	_ = os.Getenv("FAUNADB_SECRET")
+	// io.WriteString(w, "Secret: "+secret)
 }
