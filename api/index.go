2dc159b757afce9d67899338d8e4fa89a99cc365
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-13 23:08:53 +0200

    ...

--- api/index.go
+++ api/index.go
@@ -2,11 +2,18 @@ package api
 
 import (
 	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true}`)
 	w.WriteHeader(http.StatusOK)
+}
+
+func TestFauna(w http.ResponseWriter, r *http.Request) {
+	secret := os.Getenv("FAUNADB_SECRET")
+	log.Println("Secret: " + secret)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mbraeutig/godo-now
+
+go 1.14
