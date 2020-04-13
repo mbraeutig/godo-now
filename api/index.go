d3a61b0e97b4628be531e0a017f6b5694d555e8b
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-13 23:29:17 +0200

    ...

--- api/index.go
+++ api/index.go
@@ -3,9 +3,17 @@ package api
 import (
 	"net/http"
 	"os"
+
+	f "github.com/mbraeutig/faunadb-go/tree/release-2.11.0-semverfix/v2"
+
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	_ = os.Getenv("FAUNADB_SECRET")
-	// io.WriteString(w, "Secret: "+secret)
+	secret = os.Getenv("FAUNADB_SECRET")
+	adminClient := newAdminClient(secret)
+	io.WriteString(w, fmt.Sprintf("Client: %v", adminClient)
+}
+
+func newAdminClient(faunaSecret string) *f.FaunaClient {
+	return f.NewFaunaClient(faunaSecret)
 }
