7c7eb5fe44b2f4a234ad28996b012b01f259621d
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-15 17:57:47 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -5,29 +5,14 @@ import (
 	"net/http"
 
 	"github.com/mbraeutig/godo-now/api"
-	// "github.com/mbraeutig/godo-now/api"
 )
 
 func main() {
-
-	// c := fauna.Client()
-	// all, err := todo.ReadAll(c)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// log.Printf("all:%+v", len(all))
-
-	// for _, t := range all {
-	// 	log.Println("! ", t)
-	// }
-
-	// log.Printf("all:%+v", all[0])
-
 	http.HandleFunc("/index", api.Index)
 	http.HandleFunc("/health", api.Health)
 	http.HandleFunc("/create", api.Create)
-	http.HandleFunc("/delete", api.Delete)
 	http.HandleFunc("/read", api.Read)
 	http.HandleFunc("/readall", api.ReadAll)
+	http.HandleFunc("/delete", api.Delete)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
